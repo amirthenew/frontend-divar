323afa9e523460f958b31853d375f639d3ca8f61
amirthenew <ah.abshari@hotmail.com>
2024-02-01 13:14:04 +0330

    setstate for swap between senotp and checkotp

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import CheckOtpForm from "../components/templates/CheckOtpForm";
 import SendOtpForm from "../components/templates/SendOtpForm";
 
 const AuthPage = () => {
+
+const [step,setStep]=useState(2)
+
     return ( 
 <>
-<SendOtpForm/>
-<CheckOtpForm/>
+{step===1 && <SendOtpForm/>}
+{step===2 && <CheckOtpForm/>}
 </>
      );
 }
