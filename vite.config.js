bd587482e63018422cf2b26b256f92ea4289040c
amirthenew <ah.abshari@hotmail.com>
2024-02-02 18:58:31 +0330

    absoloute import

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-const paths = [
-"src",
-"assets",
-"components",
-"configs",
-"constants",
-"pages",
-"router",
-"services",
-"styles",
-"utils"
-];
+import { paths } from './src/constants/paths'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
