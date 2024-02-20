523d1043de0a316897ca868135f4146cada67776
amirthenew <ah.abshari@hotmail.com>
2024-02-20 17:01:25 +0330

    addcategory cretaed

--- src/components/templates/CategoryForm.jsx
+++ src/components/templates/CategoryForm.jsx
@@ -1,18 +1,31 @@
 import { useState } from "react";
 import styles from './CategoryForm.module.css'
+import { useMutation } from "@tanstack/react-query";
+import { addCategory } from "src/services/admin";
 const CategoryForm = () => {
+
 const [form,setForm]=useState({name:'',slug:'',icon:''})
+
+const {mutate,isLoading,error,data}=useMutation(addCategory)
+
+console.log({isLoading,error,data})
+
 const changeHandler = (event) =>{
     setForm({...form,[event.target.name]:event.target.value})
 }
+
 const submitHandler = (event)=>{
 event.preventDefault()
+
+if (!form.name || !form.slug || !form.icon) return;
+mutate(form)
 console.log(form)
 }
     return (
     
     <form className={styles.form} onChange={changeHandler} onSubmit={submitHandler}>
         <h3>دسته بندی جدید</h3>
+        {data?.status ===201 && <p>دسته بندی با موفقیت اضافه شد</p>}
         <label htmlFor="name">اسم دسته بندی</label>
         <input type="text" name="name" id="name"/>
         <label htmlFor="slug">اسلاگ</label>
---
+++ src/services/admin.js
@@ -0,0 +1,6 @@
+import api from "src/configs/api"
+
+const addCategory = data=>api.post('category',data)
+
+
+export {addCategory}
